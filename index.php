159fc6f5217373d7091a60f9ccaff5d00633fe52
Zaid Bin Khalid <MianZaid@users.noreply.github.com>
2019-02-15 22:45:27 +0900

    Add files via upload

---
+++ index.php
@@ -0,0 +1,199 @@
+<?php include_once('config.php'); include('paginator.class.php'); ?>
+<!doctype html>
+<html lang="en-US" xmlns:fb="https://www.facebook.com/2008/fbml" xmlns:addthis="https://www.addthis.com/help/api-spec"  prefix="og: http://ogp.me/ns#" class="no-js">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<title>PHP pagination class with Bootstrap 4</title>
+	
+	<link rel="shortcut icon" href="https://demo.learncodeweb.com/favicon.ico">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous">
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+	<!--[if lt IE 9]>
+	<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+	<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+	<![endif]-->
+	<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+	<script>
+	  (adsbygoogle = window.adsbygoogle || []).push({
+		google_ad_client: "ca-pub-6724419004010752",
+		enable_page_level_ads: true
+	  });
+	</script>
+	<!-- Global site tag (gtag.js) - Google Analytics -->
+	<script async src="https://www.googletagmanager.com/gtag/js?id=UA-131906273-1"></script>
+	<script>
+	  window.dataLayer = window.dataLayer || [];
+	  function gtag(){dataLayer.push(arguments);}
+	  gtag('js', new Date());
+	  gtag('config', 'UA-131906273-1');
+	</script>
+</head>
+
+<body>
+	
+	<div class="bg-light border-bottom shadow-sm sticky-top">
+		<div class="container">
+			<header class="blog-header py-1">
+				<nav class="navbar navbar-expand-lg navbar-light bg-light"> <a class="navbar-brand text-muted p-0 m-0" href="https://learncodeweb.com"><img src='https://learncodeweb.com/wp-content/uploads/2019/01/logo.png' alt='LearnCodeWeb'></a>
+					<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"> <span class="navbar-toggler-icon"></span> </button>
+					<div class="collapse navbar-collapse" id="navbarSupportedContent">
+						<ul class="navbar-nav mr-auto">
+							<li itemscope="itemscope" itemtype="https://www.schema.org/SiteNavigationElement" id="menu-item-17" class="active nav-item"><a title="Home" href="https://learncodeweb.com/" class="nav-link">Home</a></li>
+							<li itemscope="itemscope" itemtype="https://www.schema.org/SiteNavigationElement" id="menu-item-16" class="nav-item"><a title="Web Development" href="https://learncodeweb.com/learn/web-development/" class="nav-link">Web Development</a></li>
+							<li itemscope="itemscope" itemtype="https://www.schema.org/SiteNavigationElement" id="menu-item-558" class="nav-item"><a title="PHP" href="https://learncodeweb.com/learn/php/" class="nav-link">PHP</a></li>
+							<li itemscope="itemscope" itemtype="https://www.schema.org/SiteNavigationElement" id="menu-item-14" class="nav-item"><a title="Bootstrap" href="https://learncodeweb.com/learn/bootstrap-framework/" class="nav-link">Bootstrap</a></li>
+							<li itemscope="itemscope" itemtype="https://www.schema.org/SiteNavigationElement" id="menu-item-559" class="nav-item"><a title="WordPress" href="https://learncodeweb.com/learn/wordpress/" class="nav-link">WordPress</a></li>
+							<li itemscope="itemscope" itemtype="https://www.schema.org/SiteNavigationElement" id="menu-item-15" class="nav-item"><a title="Snippets" href="https://learncodeweb.com/learn/snippets/" class="nav-link">Snippets</a></li>
+						</ul>
+						<form method="get" action="https://learncodeweb.com" class="form-inline my-2 my-lg-0">
+							<div class="input-group input-group-md">
+								<input type="text" class="form-control search-width" name="s" id="search" value="" placeholder="Search..." aria-label="Search">
+								<div class="input-group-append">
+									<button type="submit" class="btn btn-primary" id="searchBtn"><i class="fa fa-search"></i></button>
+								</div>
+							</div>
+						</form>
+					</div>
+				</nav>
+			</header>
+		</div> <!--/.container-->
+	</div>
+	<div class="container my-2">
+		<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+		<!-- demo top banner -->
+		<ins class="adsbygoogle"
+			 style="display:block"
+			 data-ad-client="ca-pub-6724419004010752"
+			 data-ad-slot="6737619771"
+			 data-ad-format="auto"
+			 data-full-width-responsive="true"></ins>
+		<script>
+		(adsbygoogle = window.adsbygoogle || []).push({});
+		</script>
+	</div>
+	
+	<div class="container">
+		<h1><a href="https://learncodeweb.com/php/php-pagination-class-with-bootstrap-4/">PHP pagination class with Bootstrap 4</a></h1>
+    	<hr>
+		<form method="get" action="<?php echo $_SERVER['PHP_SELF'];?>" class="form-inline">
+			<select name="tb1" onchange="submit()" class="form-control">
+				<option>Please select a continent</option>
+				<?php
+					$Continentqry   =   $db->query('SELECT DISTINCT continentName FROM countries ORDER BY continentName ASC');
+					while($crow = $Continentqry->fetch_assoc()) {
+						echo "<option";
+						if(isset($_REQUEST['tb1']) and $_REQUEST['tb1']==$crow['continentName']) echo ' selected="selected"';
+						echo ">{$crow['continentName']}</option>\n";
+					}
+				?>
+			</select>
+		</form>
+		<hr>
+		<?php
+		if(isset($_REQUEST['tb1'])) {
+			$condition		=	"";
+			if(isset($_GET['tb1']) and $_GET['tb1']!="")
+			{
+				$condition		.=	" AND continentName='".$_GET['tb1']."'";
+			}
+			
+			//Main query
+			$pages = new Paginator;
+			$pages->default_ipp = 15;
+			$sql_forms = $db->query("SELECT * FROM countries WHERE 1 ".$condition."");
+			$pages->items_total = $sql_forms->num_rows;
+			$pages->mid_range = 9;
+			$pages->paginate();	
+			
+			$result	=	$db->query("SELECT * FROM countries WHERE 1 ".$condition." ORDER BY countryName ASC ".$pages->limit."");
+		}
+		?>
+		<div class="clearfix"></div>
+		
+		<div class="row marginTop">
+			<div class="col-sm-12 paddingLeft pagerfwt">
+				<?php if($pages->items_total > 0) { ?>
+					<?php echo $pages->display_pages();?>
+					<?php echo $pages->display_items_per_page();?>
+					<?php echo $pages->display_jump_menu(); ?>
+				<?php }?>
+			</div>
+			<div class="clearfix"></div>
+		</div>
+
+		<div class="clearfix"></div>
+		
+		<table class="table table-bordered table-striped">
+			<thead>
+				<tr>
+					<th>Sr#</th>
+					<th>Country Name</th>
+					<th>ID</th>
+					<th>Country Code</th>
+					<th>Currency Code</th>
+					<th>Capital</th>
+				</tr>
+			</thead>
+			<tbody>
+				<?php 
+				if($pages->items_total>0){
+					$n  =   1;
+					while($val  =   $result->fetch_assoc()){ 
+				?>
+				<tr>
+					<td><?php echo $n++; ?></td>
+					<td><?php echo mb_strtoupper($val['countryName']); ?></td>
+					<td><?php echo $val['id']; ?></td>
+					<td><?php echo mb_strtoupper($val['countryCode']); ?></td>
+					<td><?php echo mb_strtoupper($val['currencyCode']); ?></td>
+					<td><?php echo mb_strtoupper($val['capital']); ?></td>
+				</tr>
+				<?php 
+					}
+				}else{?>
+				<tr>
+					<td colspan="6" align="center"><strong>No Record(s) Found!</strong></td>
+				</tr>
+				<?php } ?>
+			</tbody>
+		</table>
+		
+		<div class="clearfix"></div>
+		
+		<div class="row marginTop">
+			<div class="col-sm-12 paddingLeft pagerfwt">
+				<?php if($pages->items_total > 0) { ?>
+					<?php echo $pages->display_pages();?>
+					<?php echo $pages->display_items_per_page();?>
+					<?php echo $pages->display_jump_menu(); ?>
+				<?php }?>
+			</div>
+			<div class="clearfix"></div>
+		</div>
+
+		<div class="clearfix"></div>
+        
+    </div> <!--/.container-->
+	
+	<div class="container">
+		<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+		<!-- demo left sidebar -->
+		<ins class="adsbygoogle"
+			 style="display:block"
+			 data-ad-client="ca-pub-6724419004010752"
+			 data-ad-slot="7706376079"
+			 data-ad-format="auto"
+			 data-full-width-responsive="true"></ins>
+		<script>
+		(adsbygoogle = window.adsbygoogle || []).push({});
+		</script>
+	</div>
+	
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js" integrity="sha384-wHAiFfRlMFy6i5SRaxvfOCifBUQy1xHdJ/yoi7FRNXMRBu5WHdZYu1hA6ZOblgut" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js" integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k" crossorigin="anonymous"></script>
+    
+</body>
+</html>
