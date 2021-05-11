98543e804fc4dbb2be86d674b8b5fa082073175b
Mian Zaid <zaid@qualitextrading.com>
2021-05-11 09:37:57 +0900

    update composer.

--- vendor/composer/autoload_classmap.php
+++ vendor/composer/autoload_classmap.php
@@ -7,4 +7,5 @@ $baseDir = dirname($vendorDir);
 
 return array(
     'Composer\\InstalledVersions' => $vendorDir . '/composer/InstalledVersions.php',
+    'Paginator' => $baseDir . '/paginator.class.php',
 );
